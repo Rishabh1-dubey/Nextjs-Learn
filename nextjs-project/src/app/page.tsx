7a77dad783b431d7c62d4abf09fe509b4e4548b0
Rishabh Dubey <rishabhdubey006@gmail.com>
2024-10-12 21:13:59 +0530

    installing nvm and did silly mistake

---
+++ nextjs-project/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-black">
+    
+          
+        <h1 className="text-white text-3xl">hello</h1>
+    </div>
+  );
+}
