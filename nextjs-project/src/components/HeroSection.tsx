f03b27f88b4991f3c2585a19a96300defc854b2a
Rishabh Dubey <rishabhdubey006@gmail.com>
2024-10-15 22:21:16 +0530

    adding sootlight effectnad moving border

---
+++ nextjs-project/src/components/HeroSection.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Spotlight } from "./ui/Spotlight";
+import Link from "next/link";
+import { Button } from "./ui/moving-border";
+
+const HeroSection = () => {
+  return (
+    <div className="">
+      <h1 className=" text-white  mt-64 ml-42 text-center text-6xl font-bold ">Explore the best courese</h1>
+      <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="pink"
+      />
+      <p className="p-10 w-[830px] text-center ml-[370px] text-xl  mx-auto text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab in quis quo error expedita pariatur sequi exercitationem voluptatum itaque numquam deserunt harum, voluptas neque delectus excepturi ex aperiam cupiditate quas.
+      </p>
+      <div className="mt-2 text-white  text-center ">
+        <Link href={"/courses"}
+        >
+        <Button>
+          Couses
+        </Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
---
+++ nextjs-project/src/components/Navbar.tsx
@@ -0,0 +1,41 @@
+"use client";
+import React, { act, useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "../utis/cn";
+import Link from "next/link";
+
+const Navbar = ({ className }: { className?: string }) => {
+  const [active, setActive] = useState<string | null>(null);
+  return (
+    <div
+      className={cn(
+        "fixed top-10 inset-x-0 max-w-2xl mx-auto z-50 text-white",
+        className
+      )}
+    >
+      <Menu setActive={setActive}>
+        <Link href="/">
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Home"
+          ></MenuItem>
+        </Link>
+
+{/* //courses section */}
+        <MenuItem setActive={setActive} active={active} item="Courses">
+          <div className="flex flex-col space-y-4 text-sm">
+          <HoveredLink href="/web-dev">Web Development</HoveredLink>
+          <HoveredLink href="/c++">C++</HoveredLink>
+          <HoveredLink href="/java">Java Spring Boot</HoveredLink>
+          <HoveredLink href="/ds">Data Science</HoveredLink>
+          </div>
+        </MenuItem>
+{/* --------- ------------------- */}
+
+      </Menu>
+    </div>
+  );
+};
+
+export default Navbar;
