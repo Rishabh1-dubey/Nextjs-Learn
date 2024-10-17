eb598d4be1c8915b75b99415f0c9253ec073d973
Rishabh Dubey <rishabhdubey006@gmail.com>
2024-10-18 00:47:32 +0530

    adding parents files

--- nextjs-project/src/app/page.tsx
+++ nextjs-project/src/app/page.tsx
@@ -2,12 +2,14 @@ import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
 import Image from "next/image";
 import FeaturedCourses from "@/components/FeaturedCourses";
+import WhychooseUs from "@/components/WhychooseUs";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2] ">
        <HeroSection/>
        <FeaturedCourses/>
+       <WhychooseUs/>
       
     </main>
   );
