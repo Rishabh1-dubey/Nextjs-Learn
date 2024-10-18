3ef09612a775e49025336280abb8b0182fd1a569
Rishabh Dubey <rishabhdubey006@gmail.com>
2024-10-18 21:37:41 +0530

    adding parent files

--- nextjs-project/next.config.mjs
+++ nextjs-project/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{
+        domains:['encrypted-tbn0.gstatic.com','pbs.twimg.com','www.piyushgarg.dev','yt3.googleusercontent.com']
+    }
+};
 
 export default nextConfig;
