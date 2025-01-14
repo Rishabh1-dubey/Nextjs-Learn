76e6522ca14929197201ab1b8317711d48ab6d21
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-01-14 21:58:26 +0530

    adding about me sectoin

---
+++ next-auth/src/app/api/users/aboutme/route.ts
@@ -0,0 +1,19 @@
+import { getDataFromToken } from "@/helpers/detDataFromToken";
+import User from "@/models/userModels";
+import { connect } from "@/dbconfig/dbConfig";
+import { NextRequest, NextResponse } from "next/server";
+
+
+connect()
+
+
+export async function POST(request : NextRequest) {
+    const userId = await getDataFromToken(request);
+    const user = await User.findOne({_id:userId}).select("-password")
+
+    // check if there is no user
+    return NextResponse.json({
+        message:"User found",
+        data :user
+    })
+}
