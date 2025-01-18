fb198176ecb844357e340e445a4e341dfc0504e4
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-01-18 22:05:01 +0530

    getting ready data to verify our app

--- next-auth/src/app/verifyemail/page.tsx
+++ next-auth/src/app/verifyemail/page.tsx
@@ -1,10 +1,55 @@
 "use client"
-import React from 'react'
+import axios from "axios";
+import { tree } from "next/dist/build/templates/app-page";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
 
-const page = () => {
+const VerifyEmail = () => {
+  const [token, settoken] = useState("");
+  const [error, setError] = useState(false);
+  const [verified, setVerified] = useState(false);
+
+  const VerifiedEmail = async () => {
+    try {
+      await axios.post("/api/users/verifyemail", { token });
+      setVerified(true);
+    } catch (error: any) {
+      console.log(error.response.data);
+      setError(true);
+    }
+
+    useEffect(() => {
+      setError(false);
+      const userUrl = window.location.search.split("=")[1];
+      settoken(userUrl || " ");
+    }, []);
+
+    useEffect(() => {
+      setError(false);
+      if (token.length > 0) {
+        VerifiedEmail();
+      }
+    }, [token]);
+  };
+
   return (
-    <div>page</div>
-  )
-}
+    <div className=" flex flex-col items-center justify-center min-h-screen py-2s">
+      
 
-export default page
+      <h1 className="text-4xl">Verify Email</h1>
+      <h2 className="p-2 bg-orange-500  text-black w-80">
+        {token ? `${token}` : "  No token"}
+      </h2>
+    
+
+      {verified && (
+        <div>
+          <h1>Verifiedd</h1>
+          <Link href="/login">Login</Link>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default VerifyEmail;
