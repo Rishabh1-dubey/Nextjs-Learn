c2f207c2709dc1e920b2f548d6eaba2b23c0b306
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-01-18 22:04:25 +0530

    adding profile token and data

--- next-auth/src/app/profile/page.tsx
+++ next-auth/src/app/profile/page.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+"use client"
 
-const page = () => {
+import axios from 'axios'
+import {  useRouter } from 'next/router'
+import React, { useState } from 'react'
+import toast from 'react-hot-toast'
+
+const ProfilePage = () => {
+
+const router = useRouter()
+const [data, setData] = useState("Nothing")
+
+const userMe=async()=>{
+
+
+  const res  = await axios.post("api/users/me")
+  console.log(res);
+  setData(res.data._id)
+}
+
+
+const  Logout=  async()=>{
+  try {
+    
+
+    const res = await axios.post("api/users/aboutme")
+    console.log(res);
+
+    toast.success("LogOut success") 
+     router.push("/login")
+  } catch (error:any) {
+    
+    toast.error("something went wrong")
+
+
+    console.log(error.message)
+  }
+}
+
   return (
     <div>page</div>
   )
 }
 
-export default page
+export default ProfilePage
